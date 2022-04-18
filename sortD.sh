41f48ca49c68945f3b5f6c54dbb8878ce51e1d08
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2022-04-18 11:38:52 +0530

    Update sortD.sh

--- sortD.sh
+++ sortD.sh
@@ -31,8 +31,7 @@ uniqSU() {
 
 #Phonic.
 phonic(){
-	paste -d "" onlyE_fw.txt onlyD_fw.txt > phonictmp.txt #Pasting the two separate files of English and Devanagari with a delimiter of no space.
-	cat phonictmp.txt | perl -CSD -Mutf8 -pe 's/(?<=[अ-ह\p{M}])(?=[^अ-ह\p{M}])|(?<=[^अ-ह\p{M}])(?=[अ-ह])/ /g' > phonic.txt #Using Perl with a regex expression to leave space for before and after of Devanagari text
+	cat temp.txt | perl -CSD -Mutf8 -pe 's/(?<=[अ-ह\p{M}])(?=[^अ-ह\p{M}])|(?<=[^अ-ह\p{M}])(?=[अ-ह])/ /g' > phonic.txt #Using Perl with a regex expression to leave space for before and after of Devanagari text
 }
 
 #Combining all the text files in a CSV file.
